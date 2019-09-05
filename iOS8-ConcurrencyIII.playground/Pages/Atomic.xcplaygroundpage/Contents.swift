ac72e98f6217dac2a760fc9bdfa811054d869cdf
Paul Solt <PaulSolt@gmail.com>
2019-09-05 13:18:26 -0400

    Added atomic data structure

--- iOS8-ConcurrencyIII.playground/Pages/Atomic.xcplaygroundpage/Contents.swift
+++ iOS8-ConcurrencyIII.playground/Pages/Atomic.xcplaygroundpage/Contents.swift
@@ -2,6 +2,40 @@
 
 import Foundation
 
-var str = "Hello, playground"
+// Serial = work happens in order (like following a series of steps in a recipe)
+// Concurrent = work happens all at the same time (10 workers picking blueberries)
+
+// Sync = work happens now (fire alarm -> leave the building)
+// Async = work happens at some point in the future (apply to an iOS job)
+
+
+class Atomic<Element> {
+    private var value: Element
+    private var queue = DispatchQueue(label: "SerialQueue")
+    
+    init(value: Element) {
+        self.value = value
+    }
+    
+    func modify(_ modifier: (Element) -> Element) {
+        queue.sync {
+            value = modifier(value)
+        }
+    }
+    
+    func get() -> Element {
+        return queue.sync { value }
+    }
+}
+
+let sharedValue = Atomic(value: 10)
+//let sharedValue = Atomic(value: "Hello")
+
+DispatchQueue.concurrentPerform(iterations: 100) { (threadNumber) in
+    sharedValue.modify { $0 + 1 }
+}
+
+sharedValue.get()
+
 
 //: [Next](@next)
