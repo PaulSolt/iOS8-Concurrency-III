c51e0daf6150f7e62d9b4977875c3b0ddc05e086
Paul Solt <PaulSolt@gmail.com>
2019-09-05 12:47:41 -0400

    Concurrent queues demo

--- iOS8-BasicQueues/iOS8-BasicQueues/main.swift
+++ iOS8-BasicQueues/iOS8-BasicQueues/main.swift
@@ -8,14 +8,49 @@
 
 import Foundation
 
-let queue = DispatchQueue(label: "MyBackgroundQueue")
+let queue = DispatchQueue(label: "MyBackgroundSerialQueue") // does 1 thing at a time
+
+//print("1. I'm on the main thread")
+//
+//queue.sync { // what happens if this is sync?
+//    print("2.1 Downloading images in the background")
+//
+//    usleep(5_000_000) // 1 second
+////    usleep(100) // microseconds
+//    print("2.2 Downloading images in the background")
+//}
+//
+//print("3. I'm finished")
+
+
+let concurrentQueue = DispatchQueue(label: "MyBackgroundConcurrentQueue", attributes: [.concurrent])
 
 print("1. I'm on the main thread")
-
-queue.async {
-//    usleep(1_000_000) // 1 second
-    usleep(100) // microseconds
-    print("2. Downloading images in the background")
+concurrentQueue.async { // what happens if this is sync?
+    print("2.1 Downloading images in the background")
+    
+    usleep(1_000)
+    print("2.2 Downloading images in the background")
 }
 
-print("3. I'm finished")
+print("3. I'm on the main thread")
+concurrentQueue.async { // what happens if this is sync?
+    print("4.1 Downloading images in the background")
+    
+    usleep(1_000)
+    print("4.2 Downloading images in the background")
+}
+
+print("5. I'm on the main thread")
+concurrentQueue.async { // what happens if this is sync?
+    print("6.1 Downloading images in the background")
+    
+    usleep(1_000)
+    print("6.2 Downloading images in the background")
+}
+
+print("7. I'm finished")
+
+
+// Keep the Terminal app alive (indenfinitely)
+RunLoop.current.run()
